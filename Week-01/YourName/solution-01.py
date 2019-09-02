74c5eb43c2b9dae859463a4fba980771637761b1
ssdcsliet <54770935+ssdcsliet@users.noreply.github.com>
2019-09-02 22:36:53 +0530

    Delete solution-01.py

--- Week-01/YourName/solution-01.py
+++
@@ -1,6 +0,0 @@
-for _ in range(int(input())):
-    N1 = int(input())
-    arr1 = list(map(int, input().split()))
-    N2 = int(input())
-    arr2 = list(map(int, input().split()))
-    print(max(arr1)*min(arr2))
