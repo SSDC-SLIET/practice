17418251aaa5a8eca74756b5541fcb8b16e96da7
rohit0603kumar <54748370+rohit0603kumar@users.noreply.github.com>
2019-09-05 18:18:16 +0530

    Update solution--01.py

--- Week-01/Solution/RohitKumar (GCS--1940022)/solution--01.py
+++ Week-01/Solution/RohitKumar (GCS--1940022)/solution--01.py
@@ -1,1 +1,9 @@
-
+  
+#code
+n=int(input())
+for i in range(n):
+    num1=int(input())
+    array1=list(map(int,input().split()))
+    num2=int(input())
+    array2=list(map(int,input().split()))
+    print(max(array1)*min(array2))
