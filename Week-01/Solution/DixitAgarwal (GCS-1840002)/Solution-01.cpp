0fa30ca08c70c78760aee878001077daa02b621e
dixit1901 <54752633+dixit1901@users.noreply.github.com>
2019-09-06 02:32:00 +0530

    Update Solution-01.cpp

--- Week-01/Solution/DixitAgarwal (GCS-1840002)/Solution-01.cpp
+++ Week-01/Solution/DixitAgarwal (GCS-1840002)/Solution-01.cpp
@@ -1,1 +1,47 @@
- 
+ #include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    for(int i=1;i<=t;i++)
+    {
+        int min,max;
+        long n1,n2;
+        cin>>n1;
+        for(long j=0;j<n1;j++)
+        {
+            long a;
+            if(j==0)
+            {
+                cin>>a;
+                max=a;
+            }
+            else
+            {
+                cin>>a;
+                if(a>max)
+                max=a;
+            }
+        }
+        cin>>n2;
+        for(long j=0;j<n2;j++)
+        {
+            long a;
+            if(j==0)
+            {
+                cin>>a;
+                min=a;
+            }
+            else
+            {
+                cin>>a;
+                if(a<min)
+                min=a;
+            }
+        }
+        cout<<min*max<<endl;
+    }
+	//code
+	return 0;
+}
