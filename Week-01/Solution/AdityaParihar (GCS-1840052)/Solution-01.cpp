7881b41c47492dcd14f1886a35105614da3b534a
Aditya9Parihar <54755692+Aditya9Parihar@users.noreply.github.com>
2019-09-07 02:00:32 +0530

    Update Solution-01.cpp

--- Week-01/Solution/AdityaParihar (GCS-1840052)/Solution-01.cpp
+++ Week-01/Solution/AdityaParihar (GCS-1840052)/Solution-01.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int i,t;
+    cin>>t;
+    for(i=0;i<t;i++)
+    {
+        int n1,n2,max,min;
+        int* a=NULL;
+        int* arr=NULL;
+        cin>>n1;
+        a=new int[n1];
+        for(int j=0;j<n1;j++)
+        cin>>a[j];
+        max=a[0];
+        cin>>n2;
+        arr=new int[n2];
+        for(int j=0;j<n2;j++)
+        cin>>arr[j];
+        min=arr[0];
+        for(int j=0;j<n1;j++)
+        {
+            if(a[j]>max)
+            max=a[j];
+        }    
+        for(int j=0;j<n2;j++)
+        {
+            if(min>arr[j])
+            min=arr[j];
+        }
+        cout<<max*min<<endl;
+    }
+	//code
+	return 0;
+}
