482efadbcdbc6d90cfdc4c49b2cb408213fc837a
Aditya9Parihar <54755692+Aditya9Parihar@users.noreply.github.com>
2019-09-11 23:24:43 +0530

    Update Solution-05.cpp

--- Week-01/Solution/AdityaParihar (GCS-1840052)/Solution-05.cpp
+++ Week-01/Solution/AdityaParihar (GCS-1840052)/Solution-05.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+      int i,t;
+    cin>>t;
+    for(i=0;i<t;i++)
+    {
+        int n,k;
+        int count=0;
+        int* a;
+        cin>>n;
+        a=new int[n];
+        for(int j=0;j<n;j++)
+        {
+            cin>>a[j];
+        }
+        cin>>k;
+        int low=0;
+        int high=n-1;
+        while(low<high)
+        {
+            if(a[low]+a[high]==k)
+            {
+                cout<<a[low]<<" "<<a[high]<<" "<<k<<endl;
+                count++;
+            }
+            (a[low]+a[high] < k)?low++:high--;
+        }
+        if(count==0)
+        cout<<-1<<endl;
+    }
+	//code
+	return 0;
+}
