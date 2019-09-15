fd9a38b183877b75c07b18ac9864f5f492964193
amitkrraj <54941979+amitkrraj@users.noreply.github.com>
2019-09-15 22:43:03 +0530

    Update Solution 01.cpp

--- Week-01/Solution/AmitKumar (GCS-1840038)/Solution 01.cpp
+++ Week-01/Solution/AmitKumar (GCS-1840038)/Solution 01.cpp
@@ -1,6 +1,35 @@
 #include<iostream>
 using namespace std;
-main()
-{
- cout<<"Hii";
+int main()
+ {
+	long int t,i,j;
+	cin>>t;
+	for(j=0; j<t; j++)
+	{
+	    long int n1;
+	    cin>>n1;
+	    long int a[n1];
+	    for(i=0; i<n1; i++)
+	     cin>>a[i];
+	     
+	    long int n2;
+	    cin>>n2;
+	    long int b[n2];
+	    for(i=0; i<n2; i++)
+	     cin>>b[i];
+	     
+	    long int max=a[0];
+	    for(i=0; i<n1; i++){
+	        if(a[i]>max)
+	         max=a[i];
+	      }
+	    
+	    long int min=b[0];
+	    for(i=0; i<n2; i++){
+	        if(min>b[i])
+	         min=b[i];
+	      }
+	    cout<<(max*min)<<endl;
+	}
+	return 0;
 }
