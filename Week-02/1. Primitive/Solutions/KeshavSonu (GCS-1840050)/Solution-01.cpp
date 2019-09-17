6ca64ef5156d177a43ad844514458a5ca69cf1e8
kksonu <54762975+kksonu@users.noreply.github.com>
2019-09-17 22:44:54 +0530

    Create Solution-01.cpp

---
+++ Week-02/1. Primitive/Solutions/KeshavSonu (GCS-1840050)/Solution-01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	//code
+	int T;
+	cin>>T;
+	for(int i=0;i<T;i++)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    cout<<b<<" "<<a<<endl;
+	}
+	return 0;
+}
