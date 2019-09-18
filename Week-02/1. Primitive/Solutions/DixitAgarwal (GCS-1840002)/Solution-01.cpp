2f78c48fa85824feaad3a4a25edfb4668633ce95
dixit1901 <54752633+dixit1901@users.noreply.github.com>
2019-09-18 14:56:16 +0530

    Update Solution-01.cpp

--- Week-02/1. Primitive/Solutions/DixitAgarwal (GCS-1840002)/Solution-01.cpp
+++ Week-02/1. Primitive/Solutions/DixitAgarwal (GCS-1840002)/Solution-01.cpp
@@ -1,1 +1,16 @@
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    for(int i=1;i<=t;i++)
+    {
+        long a[2];
+        cin>>a[0]>>a[1];
+        cout<<a[1]<<" "<<a[0]<<endl;
+    }
+	//code
+	return 0;
+}
