82785b96045b7eb404ad462549a9c845cf87ffd2
gyanendrarawat <54751231+gyanendrarawat@users.noreply.github.com>
2019-09-18 14:29:45 +0530

    Update solution-01.c

--- Week-02/1. Primitive/Solutions/GyanendraRawat (GCS-1940016)/solution-01.c
+++ Week-02/1. Primitive/Solutions/GyanendraRawat (GCS-1940016)/solution-01.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+
+int main() 
+{
+	//code
+	int t,i,a,b;
+	scanf("%d",&t);
+	for(i=0;i<t;i++)
+	{
+	    scanf("%d %d",&a,&b);
+	    a=a+b;
+	    b=a-b;
+	    a=a-b;
+	    printf("%d %d",a,b);printf("\n");
+	    
+	}
+	return 0;
+}
