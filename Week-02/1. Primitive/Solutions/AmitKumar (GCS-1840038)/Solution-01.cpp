90e986d1d1389b7dd4d7de76c8a4c015504c364a
amitkrraj <54941979+amitkrraj@users.noreply.github.com>
2019-09-18 21:43:46 +0530

    Update Solution-01.cpp

--- Week-02/1. Primitive/Solutions/AmitKumar (GCS-1840038)/Solution-01.cpp
+++ Week-02/1. Primitive/Solutions/AmitKumar (GCS-1840038)/Solution-01.cpp
@@ -1,1 +1,17 @@
-Hello
+#include<iostream>
+using namespace std;
+int main()
+ {
+	int T,i;
+	long int a,b;
+	cin>>T;
+	for(i=0; i<T; i++){
+	    cin>>a>>b;
+	    long int x=b;
+	    b=a;
+	    a=x;
+	    cout<<a<<" "<<b;
+	    cout<<endl;
+	}
+	return 0;
+}
