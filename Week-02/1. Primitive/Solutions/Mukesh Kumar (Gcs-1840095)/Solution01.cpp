8a3953688f6328e786be134c6ddb58bc704be9fe
Mukesh Kumar <54750151+krmukesh02016@users.noreply.github.com>
2019-09-18 12:04:17 +0530

    Create Solution01.cpp

---
+++ Week-02/1. Primitive/Solutions/Mukesh Kumar (Gcs-1840095)/Solution01.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+	//code
+	int t,i;
+	cin>>t;
+	for(i=1;i<=t;i++)
+	{
+    long int a,b;
+	cin>>a;
+	cin>>b;
+	cout<<b<<" "<<a<<" "<<endl;
+	}
+	return 0;
+ }
