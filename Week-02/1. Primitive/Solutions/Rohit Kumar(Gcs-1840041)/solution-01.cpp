9e1b1adfc28decf03f4ad4e61342bf2d557cc898
rohitmantu2 <53420093+rohitmantu2@users.noreply.github.com>
2019-09-19 09:28:40 +0530

    Update solution-01.cpp

--- Week-02/1. Primitive/Solutions/Rohit Kumar(Gcs-1840041)/solution-01.cpp
+++ Week-02/1. Primitive/Solutions/Rohit Kumar(Gcs-1840041)/solution-01.cpp
@@ -1,1 +1,22 @@
 
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	int i;
+	for(i=0;i<t;i++)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    int temp;
+	    temp=b;
+	    b=a;
+	    a=temp;
+	    cout<<a<<" "<<b<<endl;
+	}
+	return 0;
+}
