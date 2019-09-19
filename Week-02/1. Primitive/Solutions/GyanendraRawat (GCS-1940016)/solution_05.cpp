007b5afafdfc8080a69bbdc24a7758b3cd40c524
gyanendrarawat <54751231+gyanendrarawat@users.noreply.github.com>
2019-09-20 01:26:06 +0530

    Update solution_05.cpp

--- Week-02/1. Primitive/Solutions/GyanendraRawat (GCS-1940016)/solution_05.cpp
+++ Week-02/1. Primitive/Solutions/GyanendraRawat (GCS-1940016)/solution_05.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+    int t,i,j;
+    cin>>t;
+for(i=0;i<t;i++)
+{  
+    string str;
+    cin>>str;
+    
+    std::reverse(std::begin(str),std::end(str));
+    
+    cout << str <<endl;
+    
+    
+}
+
+	//code
+	return 0;
+}
