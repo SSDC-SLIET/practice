4a805e8d13950e652974c936d8d69147b5af3cc8
Aditya9Parihar <54755692+Aditya9Parihar@users.noreply.github.com>
2019-09-21 01:03:21 +0530

    Update Solution-03.cpp

--- Week-02/1. Primitive/Solutions/AdityaParihar (GCS-1840052)/Solution-03.cpp
+++ Week-02/1. Primitive/Solutions/AdityaParihar (GCS-1840052)/Solution-03.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int i,t;
+    cin>>t;
+    for(i=0;i<t;i++)
+    {
+        int n;
+        cin>>n;
+        int* a;
+        a=new int[n];
+        for(int j=0;j<n;j++)
+        cin>>a[j];
+        for(int j=n-1;j>=0;j--)
+        cout<<a[j]<<" ";
+        cout<<endl;
+    }
+	//code
+	return 0;
+}
