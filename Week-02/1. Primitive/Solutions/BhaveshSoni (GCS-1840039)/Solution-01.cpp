62e542f366045366ade4f8601aea99102b94f2d9
bhavesh65321 <54760667+bhavesh65321@users.noreply.github.com>
2019-09-20 21:22:17 +0530

    Create Solution-01.cpp

---
+++ Week-02/1. Primitive/Solutions/BhaveshSoni (GCS-1840039)/Solution-01.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+     int n;
+     cin>>n;
+     long long int a,b,i;
+     for(i=1;i<=n;i++)
+     {
+         cin>>a>>b;
+         
+         cout<<b<<" "<<a<<endl;
+     }
+	//code
+	return 0;
+}
