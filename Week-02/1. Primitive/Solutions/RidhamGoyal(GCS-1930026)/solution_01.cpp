3a1750e074d3b6e831ce43ced2bf6ef0fcca469a
Ridham Goyal <54924538+ridhamgoyal07@users.noreply.github.com>
2019-09-20 21:35:50 +0530

    Create solution_01.cpp

---
+++ Week-02/1. Primitive/Solutions/RidhamGoyal(GCS-1930026)/solution_01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	unsigned long long int t;
+	cin>>t;
+	while(t){
+	    unsigned long long int a,b;
+	    cin>>a>>b;
+	    cout<<b<<" "<<a<<endl;
+	    t--;
+	    
+	}
+	return 0;
+}
