8cbd5106fe5adb4b0b12d671645200f78605713d
amitkrraj <54941979+amitkrraj@users.noreply.github.com>
2019-09-21 08:01:02 +0530

    Update Solution-05.cpp

--- Week-02/1. Primitive/Solutions/AmitKumar (GCS-1840038)/Solution-05.cpp
+++ Week-02/1. Primitive/Solutions/AmitKumar (GCS-1840038)/Solution-05.cpp
@@ -1,1 +1,21 @@
-
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+ {
+	int t;
+	cin>>t;
+	cin.ignore();
+	while(t--)
+	{
+	  string s;
+	  getline(cin,s);
+	  reverse(begin(s),end(s));
+	  int l;
+	  l=s.size();
+	  for(int i=0; i<l; i++)
+	    cout<<s[i];
+	  cout<<endl;
+	}
+	return 0;
+}
