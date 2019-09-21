a545b40d8a5531200d1bc26de0aa18c719cd616c
amitkrraj <54941979+amitkrraj@users.noreply.github.com>
2019-09-21 07:48:19 +0530

    Update Solution-03.cpp

--- Week-02/1. Primitive/Solutions/AmitKumar (GCS-1840038)/Solution-03.cpp
+++ Week-02/1. Primitive/Solutions/AmitKumar (GCS-1840038)/Solution-03.cpp
@@ -1,1 +1,23 @@
-hello
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+ {
+	int T;
+	cin>>T;
+	for(int i=0; i<T; i++)
+	{
+	    int n;
+	    cin>>n;
+	    
+	    int a[n];
+	    for(int i=0; i<n; i++)
+	        cin>>a[i];
+	    for(int i=0, j=n-1; i<j; i++, j--)
+	       swap(a[i],a[j]);
+	   
+	    for(int i=0; i<n; i++)
+	        cout<<a[i]<<" ";
+	        cout<<endl;
+	}
+}
