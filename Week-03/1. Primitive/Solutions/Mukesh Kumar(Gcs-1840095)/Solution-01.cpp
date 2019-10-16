dd43faa228bec8ae647a5abcc938f384962c7b73
Mukesh Kumar <54750151+krmukesh02016@users.noreply.github.com>
2019-10-16 19:40:51 +0530

    Create Solution-01.cpp

---
+++ Week-03/1. Primitive/Solutions/Mukesh Kumar(Gcs-1840095)/Solution-01.cpp
@@ -0,0 +1,13 @@
+
+/*This is a function problem.You only need to complete the function given below*/
+int convertFive(int n)
+{ 
+    if (n == 0) 
+        return 0; 
+ 
+    int digit = n % 10; 
+    if (digit == 0) 
+        digit = 5; 
+  
+     return convertFive(n/10)*10 +digit; 
+} 
