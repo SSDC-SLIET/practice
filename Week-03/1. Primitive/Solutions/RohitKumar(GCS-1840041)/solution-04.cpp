996641910814b591572818269c1caa874f8945d1
rohitmantu2 <53420093+rohitmantu2@users.noreply.github.com>
2019-10-22 07:04:42 +0530

    Create solution-04.cpp

---
+++ Week-03/1. Primitive/Solutions/RohitKumar(GCS-1840041)/solution-04.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,k;
+	    cin>>n>>k;
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	    {
+	        cin>>a[i];
+	    }
+	    int m=0;
+	    for(int i=0;i<n;i++)
+	    {
+	        for(int j=i+1;j<n;j++)
+	        {
+	            if(a[i]+a[j]==k)
+	            m++;
+	        }
+	    }
+	    cout<<m<<endl;
+	}
+	return 0;
+}
