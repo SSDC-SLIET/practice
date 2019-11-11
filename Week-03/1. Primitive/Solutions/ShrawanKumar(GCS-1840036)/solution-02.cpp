c7d2cfc4074052210772e1626953e5834ca9adc8
Shrawan Kumar <53372247+shrawan7@users.noreply.github.com>
2019-11-11 19:41:02 +0530

    Create solution-02.cpp

---
+++ Week-03/1. Primitive/Solutions/ShrawanKumar(GCS-1840036)/solution-02.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int convertFive(int n);
+ 
+// Driver program to test above function
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+    	int n;
+    	cin>>n;
+    	cout<<convertFive(n)<<endl;
+    }
+}
+}
+int convertFive(int n)
+{
+    if(n==0)
+      return 0;
+    int r=n%10;
+    if(r==0)
+      r=5;
+      
+    return convertFive(n/10)*10+r;
+}
