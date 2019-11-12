36d14aaeb2c9dcf332b93e7a98371b6bdab9968a
bhavesh65321 <54760667+bhavesh65321@users.noreply.github.com>
2019-11-13 00:42:14 +0530

    Create Solution 03.cpp

---
+++ Week-03/1. Primitive/Solutions/BhaveshSoni (GCS-1840039)/Solution 03.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    long n,p;
+    cin>>t;
+    for(long i=1;i<=t;i++)
+     {
+         cin>>n;
+         int a[n];
+         for(long i=0;i<n;i++)
+           {
+             cin>>a[i];
+           }
+         int max1=a[0];
+         for(long i=0;i<n;i++)
+           {
+             if(a[i]>=max1)
+               {
+                 max1=a[i];
+                 p=i;
+              }
+           }
+         a[p]=-1;
+         int max2=a[0];
+         for(int i=0;i<n;i++)
+           {
+             if(a[i]>=max2)
+              {
+               max2=a[i];
+              }
+           }
+           cout<<max1*max2<<endl;
+    }
+	//code
+	return 0;
+}
