cf7977ebf9989e08133ac45ad2dd4a73680bf13e
dixit1901 <54752633+dixit1901@users.noreply.github.com>
2019-11-13 14:13:15 +0530

    Update solution-01.cpp

--- Week-04/1. Primitive/Solutions/DixitAgarwal (GCS-1840002)/solution-01.cpp
+++ Week-04/1. Primitive/Solutions/DixitAgarwal (GCS-1840002)/solution-01.cpp
@@ -1,1 +1,18 @@
 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,sum=0;
+    cin>>n;
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(i=0;i<n;i++)
+    {
+        sum+=a[i];
+    }
+    cout<<sum;
+}
