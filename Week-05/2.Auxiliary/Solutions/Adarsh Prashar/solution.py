249e742d88a8a0ee48eac9cc9d63307967fe78a1
rockprosky <57496714+rockprosky@users.noreply.github.com>
2020-02-05 19:30:58 +0530

    Update solution.py

--- Week-05/2.Auxiliary/Solutions/Adarsh Prashar/solution.py
+++ Week-05/2.Auxiliary/Solutions/Adarsh Prashar/solution.py
@@ -1,1 +1,14 @@
-
+import numpy as np
+def rotate():
+    n=int(input())
+    entries=list(map(int,input().split()))
+    arr1=np.array(entries).reshape(n,n)
+    arr1=list(zip(*arr1[::-1]))
+    for i in range(n):
+        for j in range(n):
+            print(arr1[i][j],end=" ")
+    print()
+if __name__=='__main__':
+    T=int(input())
+    for i in range(T):
+        rotate()
