6b415534c9d71a920d7de2780f994c77720dc1f6
rohitmantu2 <53420093+rohitmantu2@users.noreply.github.com>
2020-02-05 22:04:43 +0530

    Update solution-01.cpp

--- Week-05/1.Primitive/Solutions/RohitKumar(GCS-1840041)/solution-01.cpp
+++ Week-05/1.Primitive/Solutions/RohitKumar(GCS-1840041)/solution-01.cpp
@@ -1,1 +1,23 @@
 
+#include <iostream>
+using namespace std;
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int num1,num2;
+	    cin>>num1>>num2;
+	    int sum=0;
+	    sum=num1+num2;
+	    if(sum>=12)
+	    {
+	        cout<<(sum%12)<<endl;
+	    }
+	    else
+	    cout<<sum<<endl;
+	}
+	return 0;
+}
