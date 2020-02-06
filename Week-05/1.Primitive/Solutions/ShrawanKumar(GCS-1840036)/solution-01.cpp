c9c057fa485ec16f3eb1291d4ca879c8f478dbad
Shrawan Kumar <53372247+shrawan7@users.noreply.github.com>
2020-02-06 22:08:31 +0530

    Update solution-01.cpp

--- Week-05/1.Primitive/Solutions/ShrawanKumar(GCS-1840036)/solution-01.cpp
+++ Week-05/1.Primitive/Solutions/ShrawanKumar(GCS-1840036)/solution-01.cpp
@@ -1,1 +1,15 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x,y;
+        cin>>x>>y;
+        int sum = (x+y)%12;
+        cout<<sum<<endl;
+    }
+    return 0;
+}
