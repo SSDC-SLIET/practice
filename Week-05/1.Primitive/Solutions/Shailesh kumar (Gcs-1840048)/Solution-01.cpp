1c5b9bbaabbf556162de2016f0605dee9a077922
shaileshbarun <54762174+shaileshbarun@users.noreply.github.com>
2020-02-07 15:55:19 +0530

    Create Solution-01.cpp

---
+++ Week-05/1.Primitive/Solutions/Shailesh kumar (Gcs-1840048)/Solution-01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+	int main() {
+	//code
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{
+	    int num1,num2;
+	cin>>num1>>num2;
+	cout<<(num1+num2)%12<<endl;
+	}	
+	
+	return 0;
+}
