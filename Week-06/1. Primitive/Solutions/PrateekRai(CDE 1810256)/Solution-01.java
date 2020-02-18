8fa815425c7e08d71bf668e9abf25f9635b51357
Prateek kumar rai <54741353+prateekrai123@users.noreply.github.com>
2020-02-18 14:16:22 +0530

    Create Solution-01.java

---
+++ Week-06/1. Primitive/Solutions/PrateekRai(CDE 1810256)/Solution-01.java
@@ -0,0 +1,18 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+		    t--;
+		    String s = sc.next();
+		    System.out.println(s.toUpperCase());
+		}
+	}
+}
