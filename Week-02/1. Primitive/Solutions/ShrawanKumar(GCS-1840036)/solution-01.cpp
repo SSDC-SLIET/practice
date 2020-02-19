316e3a1702a64b4e2e804f3dddeb17bdbd81d348
Shrawan Kumar <53372247+shrawan7@users.noreply.github.com>
2020-02-19 22:28:28 +0530

    Create solution-01.cpp

---
+++ Week-02/1. Primitive/Solutions/ShrawanKumar(GCS-1840036)/solution-01.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+  {
+     int i,t;
+     cin>>t;
+     for(i=1;i<=t;i++)
+     {
+         long a,b;
+         cin>>a>>b;
+         cout<<b<<" "<<a<<endl;
+     }
+	return 0;
+}
