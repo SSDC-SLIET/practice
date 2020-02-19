517687c912f30e96f993597eebd7049d534d4da6
Shrawan Kumar <53372247+shrawan7@users.noreply.github.com>
2020-02-19 22:31:10 +0530

    Create solution-03.cpp

---
+++ Week-02/1. Primitive/Solutions/ShrawanKumar(GCS-1840036)/solution-03.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+	//cout<<"It's me Shrawan";
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	      cin>>a[i];
+	    for(int i=n-1;i>=0;i--)
+	      cout<<a[i]<<" ";
+	    cout<<endl;  
+	}
+	return 0;
+}
