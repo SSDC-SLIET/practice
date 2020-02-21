6692d5bb9054d24c2e914075bc373a6835811f2b
masoommittal41 <54764796+masoommittal41@users.noreply.github.com>
2020-02-21 11:52:21 +0530

    Create Week6sol2.cpp

---
+++ Week-06/1. Primitive/Solutions/MasoomMittal (GCS-1940066)/Week6sol2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{
+	    int n;
+	    cin>>n;
+	   cout<<((n)*(n+1)*(2*n+1))/6<<endl;
+	}
+	return 0;
+}
