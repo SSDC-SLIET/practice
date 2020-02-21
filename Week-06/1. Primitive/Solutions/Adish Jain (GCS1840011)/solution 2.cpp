07f0fd28abb49cc2d5f5e2414403f27a1090d350
adishjain-2knm <54764581+adishjain-2knm@users.noreply.github.com>
2020-02-22 00:00:33 +0530

    Create solution 2.cpp

---
+++ Week-06/1. Primitive/Solutions/Adish Jain (GCS1840011)/solution 2.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+ {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    long sum;
+	    sum=(n*(n+1)*(2*n+1)/6);
+	    cout<<sum<<endl;
+	}
+	return 0;
+}
